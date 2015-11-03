213db3c35012a527b7f8a023998dccfbbda277ae
shawnDu <shuaishuai.du@smartereye.com>
2015-11-03 19:54:16 +0800

    add CALayerController

--- CALayerGuide/CALayerGuide/Controller/CALayerController.swift
+++ CALayerGuide/CALayerGuide/Controller/CALayerController.swift
@@ -9,5 +9,36 @@
 import UIKit
 
 class CALayerController: UIViewController {
-
+    
+    @IBOutlet weak var layerView: UIView!
+    
+    let layer = CALayer()
+    let star = UIImage(named: "CALayer")?.CGImage
+    
+    func setupLayer() {
+        layer.frame = layerView.bounds
+        layer.contents = star
+        layer.contentsGravity = kCAGravityCenter
+        layer.geometryFlipped = false
+        layer.cornerRadius = 75
+        layer.borderWidth = 5
+        layer.borderColor = UIColor.whiteColor().CGColor
+        layer.backgroundColor = UIColor.orangeColor().CGColor
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize(width: 0, height: 3)
+        layer.shadowRadius = 2.0
+        layer.magnificationFilter = kCAFilterLinear
+    }
+    
+    override func viewDidLoad() {
+        setupLayer()
+        layerView.layer.addSublayer(layer)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+         let identifier = segue.identifier
+        if (identifier == "DisplayLayerControls") {
+            (segue.destinationViewController as? CALayerControlsViewController)?.layerViewController = self
+        }
+    }
 }
