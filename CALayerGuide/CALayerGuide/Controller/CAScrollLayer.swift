2eb929d3cc3abe983e1130fadf6a916bda985400
shawnDu <shuaishuai.du@smartereye.com>
2015-11-04 16:22:20 +0800

    add CAScrollLayer, CATextLayer

--- CALayerGuide/CALayerGuide/Controller/CAScrollLayer.swift
+++ CALayerGuide/CALayerGuide/Controller/CAScrollLayer.swift
@@ -8,6 +8,59 @@
 
 import UIKit
 
+class ScrollingView: UIView {
+    
+    override class func layerClass() -> AnyClass {
+        return CAScrollLayer.self
+    }
+    
+}
+
 class CAScrollLayerController: UIViewController {
+    
+    @IBOutlet weak var scrollingView: ScrollingView!
+    @IBOutlet weak var horizontalScrollingSwitch: UISwitch!
+    @IBOutlet weak var verticalScrollingSwitch: UISwitch!
+    
+    var scrollingViewLayer: CAScrollLayer {
+        return scrollingView.layer as! CAScrollLayer
+    }
+    
+    // MARK: - View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollingViewLayer.scrollMode = kCAScrollBoth
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func panRecognized(sender: UIPanGestureRecognizer) {
+        var newPoint = scrollingView.bounds.origin
+        newPoint.x -= sender.translationInView(scrollingView).x
+        newPoint.y -= sender.translationInView(scrollingView).y
+        sender.setTranslation(CGPointZero, inView: scrollingView)
+        scrollingViewLayer.scrollToPoint(newPoint)
+        
+        if sender.state == .Ended {
+            UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseInOut, animations: {
+                [unowned self] in
+                self.scrollingViewLayer.scrollToPoint(CGPointZero)
+                }, completion: nil)
+        }
+    }
+    
+    @IBAction func scrollingSwitchChanged(sender: UISwitch) {
+        switch (horizontalScrollingSwitch.on, verticalScrollingSwitch.on) {
+        case (true, true):
+            scrollingViewLayer.scrollMode = kCAScrollBoth
+        case (true, false):
+            scrollingViewLayer.scrollMode = kCAScrollHorizontally
+        case (false, true):
+            scrollingViewLayer.scrollMode = kCAScrollVertically
+        default:
+            scrollingViewLayer.scrollMode = kCAScrollNone
+        }
+    }
     
 }
